1f70d7bc67ced9524463b8422c02dbd4434314ba
Kevin Rubama <93825262+Lelegoyeng@users.noreply.github.com>
2023-01-20 06:24:28 +0000

    update

--- index.js
+++ index.js
@@ -25,39 +25,45 @@ loadFeatures(client)
   const itemlist = array.map((e) => {
     return e.nama;
   });
-  
+
 client.on('message', async msg => {
+    let i = 0
     switch (msg.content) {
 
       case "!start":
-      msg.channel.send("You are now subscribed to eye reminders.");
-      (async ()=> {
-        for(let i = 0; i < itemlist.length; i++){
-          console.log(i)
-          const carleon = await axios.get(
-            `https://www.albion-online-data.com/api/v2/stats/prices/${itemlist}?locations=caerleon`
-          );
-        
-          const blackmarket = await axios.get(
-            `https://www.albion-online-data.com/api/v2/stats/prices/${itemlist}?locations=blackmarket`
-          );
-          if (blackmarket?.data[0]?.buy_price_min > carleon?.data[0]?.sell_price_min) {
-            if(blackmarket?.data[0]?.buy_price_min !== 0 && carleon?.data[0]?.sell_price_min !== 0){
-              msg.channel.send(`
-              ${blackmarket?.data[0]?.item_id} - Blackmarket Price( ${blackmarket?.data[0]?.buy_price_min} ) -
-              Carleon Price ( ${carleon?.data[0]?.sell_price_min} )
-              `);
-            }
+        (async ()=> {
+          for(i; i < itemlist.length; i++){
+            console.log(i)
            
+            const carleon = await axios.get(
+              `https://www.albion-online-data.com/api/v2/stats/prices/${itemlist[i]}?locations=caerleon`
+            );
+          
+            const blackmarket = await axios.get(
+              `https://www.albion-online-data.com/api/v2/stats/prices/${itemlist[i]}?locations=blackmarket`
+            );
+            if (blackmarket?.data[0]?.buy_price_min > carleon?.data[0]?.sell_price_min) {
+              if(blackmarket?.data[0]?.buy_price_min !== 0 && carleon?.data[0]?.sell_price_min !== 0){
+                msg.channel.send(`
+                ${blackmarket?.data[0]?.item_id} - Blackmarket Price( ${blackmarket?.data[0]?.buy_price_min} ) -
+                Carleon Price ( ${carleon?.data[0]?.sell_price_min} )
+                `);
+              }
+             
+            }else{
+              console.log("berjalan")
+            }
+  
           }
 
-        }
-      })()
-
+          if(i == itemlist.length){
+            msg.channel.send("Fetch Success!");
+          }
+        })()
+      break;
+      case "!stop":
+      msg.channel.send("End!");
         break;
-        case "!stop":
-            msg.channel.send("Aktifitas Telah Berhenti");
-            break;
     }
   })
 // client.login(process.env.DLUNAR)                                             //nek gawe heroku
